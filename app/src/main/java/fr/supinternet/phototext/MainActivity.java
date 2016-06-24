ab155a314c9ba24965f0824ee31992ff40347a6f
Raphaël Sanchez <raphael.sanchez@supinternet.fr>
2016-06-25 00:44:44 +0200

    add method to change activity

--- app/src/main/java/fr/supinternet/phototext/MainActivity.java
+++ app/src/main/java/fr/supinternet/phototext/MainActivity.java
@@ -1,13 +1,23 @@
 package fr.supinternet.phototext;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+          super.onCreate(savedInstanceState);
+
+           setContentView(R.layout.activity_main);
+    }
+    
+    public void pushPseudo(View view) {
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
     }
 }
